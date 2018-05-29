dd98e6f52311a6604ae64ca202e59af839e67af3
christena weatherspoon <greatdiamondmoon@gmail.com>
2018-05-29 11:19:31 -0400

    remove stuff

--- baby_nearest/baby_nearest/urls.py
+++ baby_nearest/baby_nearest/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# from djgeojson.views import GeoJSONLayerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- baby_nearest/babynearest/models.py
+++ baby_nearest/babynearest/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 class Locations(models.Model):
-    name = models.CharField(max_length=300)
-    url = models.CharField(max_length=300)
-    address = models.CharField(max_length=300)
-    city  = models.CharField(max_length=300)
-    state  = models.CharField(max_length=300)
-    zipcode = models.CharField(max_length=300)
-    latitute = models.CharField(max_length=300)
-    longitude  = models.CharField(max_length=300)
-
+    name = models.CharField(max_length=300, default='')
+    url = models.CharField(max_length=300, null=True, blank=True)
+    address = models.CharField(max_length=300, null=True, blank=True)
+    city  = models.CharField(max_length=300, null=True, blank=True)
+    state  = models.CharField(max_length=300, db_index=True)
+    zipcode = models.CharField(max_length=300, db_index=True)
+    latitute = models.CharField(max_length=300, null=True, blank=True)
+    longitude  = models.CharField(max_length=300, null=True, blank=True)
+    # location = models.ForeignKey(GeoJson, db_index=True)
 
 class GeoJson(models.Model):
-    name = models.CharField(max_length=300)
-    url = models.CharField(max_length=300)
+    name = models.CharField(max_length=300, null=True, blank=True)
+    url = models.CharField(max_length=300, null=True, blank=True)
